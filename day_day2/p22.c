a15afa4b7a15dda01a1327a0c83291e90569a930
Pratiksha141 <pratikshakamble2019.elect@mmcoe.edu.in>
2024-11-23 11:20:18 +0530

    p22

---
+++ day_day2/p22.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<stdlib.h>
+{
+    int input_number=atoi(orgd[1]);
+    {
+        int input_number=atoi(orgs[1]);
+    for (int i=1;i<=20;i++)
+    {
+        printf("%d*%02d=%03d\n",input_number,i,input_number*i);
+    }
+    }
+}

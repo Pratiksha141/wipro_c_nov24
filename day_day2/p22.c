d2248e05b92145d8fd4c5c40ede6cc9d27db2461
Pratiksha141 <pratikshakamble2019.elect@mmcoe.edu.in>
2024-11-23 15:06:52 +0530

    even_odd_sum

--- day_day2/p22.c
+++ day_day2/p22.c
@@ -4,18 +4,18 @@
 #include<stdio.h>
 int main()
 {
- int sum=0,odd_num=1,num,digit;
+ int sum=0, odd_num=1, num=0, digit=0;
  puts("Enter a no.");
  scanf("%d",num);
  while(num!=0)
  {
-    digit=num%10;
-    num/=10;
+    digit=num%10 ;
+    num/=10 ;
     if(digit == 0 && odd_num==0)
     {
-        sum+=digit;
-    odd_num++;
+        sum+=digit ;
     }
+    odd_num++ ;
     printf("sum of numbers=%d\n",sum);
  }
 }
